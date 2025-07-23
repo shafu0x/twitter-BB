b53c756bf766f6a7f7821597eb2d08d4450433ee
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-07-22 22:41:41 -0400

    Basic setup

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: 'index.html',
+      }
+    },
+    outDir: 'dist',
+  },
+})
