dd54e36d5e4032a6df0db826c7096b532d68a88b
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-07-23 23:25:35 -0400

    Small details being setup

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+
+const buttonVariants = cva(
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed gap-2",
+    {
+      variants: {
+        variant: {
+          default:
+            "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          defaultOutline:
+            "border border-primary text-primary shadow hover:bg-primary/10",
+          destructive:
+            "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          destructiveGhost:
+            "bg-transparent text-destructive hover:bg-destructive/10 hover:text-destructive",
+          outline:
+            "border bg-transparent shadow-sm hover:bg-neutral-200 dark:hover:bg-neutral-800 hover:text-muted-foreground",
+          secondary:
+            "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/90",
+          secondaryOutline:
+            "border border-secondary text-secondary shadow-sm hover:bg-secondary/10 hover:text-secondary hover:border-secondary",
+          secondaryGhost: "bg-transparent text-secondary hover:bg-secondary/10",
+          ghost: "hover:bg-muted hover:text-muted-foreground",
+          link: "text-primary underline-offset-4 hover:underline",
+          success: "bg-green-600 text-white shadow-sm hover:bg-green-700",
+        },
+        size: {
+          default: "h-9 px-4 py-2",
+          sm: "h-8 rounded-md px-3 text-xs",
+          lg: "h-10 rounded-md px-8",
+          icon: "h-9 w-9",
+          xs: "h-6 rounded-md px-1 text-xs gap-1",
+        },
+      },
+      defaultVariants: {
+        variant: "default",
+        size: "default",
+      },
+    },
+  );
+
+function Button({
+    className,
+    variant,
+    size,
+    asChild,
+    ...props
+}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean }) {
+    const Comp = asChild ? Slot : "button";
+
+    return (
+        <Comp
+            data-slot="button"
+            className={cn(buttonVariants({ variant, size, className }))}
+            {...props}
+        />
+    )
+}
+
+export { Button, buttonVariants };
